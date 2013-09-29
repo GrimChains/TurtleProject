6f2ee156c6434ff3ef68f4747fce1449b2c4e2b3
Nick <ngg3vm@virginia.edu>
2013-09-28 22:41:18 -0400

    D4

--- ngg3vm/src/token.h
+++ ngg3vm/src/token.h
@@ -24,22 +24,21 @@
 #define OPEN 279
 #define CLOSE 280
 #define ASSIGN 281
-#define WHILE 282
-#define IF 283
-#define FUNC 284
-#define ELSE 285
-#define THEN 286
-#define OCURLY 287
-#define CCURLY 288
-#define NOTEQUAL 289
-#define EQUALSEQUALS 290
-#define LESSER 291
-#define LESSEREQUAL 292
-#define GREATER 293
-#define GREATEREQUAL 294
+#define WHILE 280
+#define EQUALS 281
+#define OR 282
+#define AND 283
+#define NOTEQUALS 284
+#define LESSEQUALS 285
+#define MOREEQUALS 286
+#define LESS 287
+#define MORE 288
+#define IF 289
+#define ELSEIF 290
+#define ELSE 291
 
 typedef union YYSTYPE
 { int i; node *n; double d;}
         YYSTYPE;
-YYSTYPE yylval;
+extern YYSTYPE yylval;
 
